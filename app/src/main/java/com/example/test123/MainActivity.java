9c3acf721ec3ee1e11db83912481e745124f20f6
luutuanhue <tuanluu@mail.klb.com.tw>
2023-02-11 14:30:33 +0700

    14.233

--- app/src/main/java/com/example/test123/MainActivity.java
+++ app/src/main/java/com/example/test123/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    //12345688878907 testmoi123789
+    //12345688878907 testmoi123789123
     //test may B
 }
